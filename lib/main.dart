93d7eb8a6393c86c03e097424af1735884e7f052
theCodeMonster1 <amharmohamed@icloud.com>
2025-03-01 07:49:27 +0530

    Disable debug banner in main.dart for a cleaner UI during development

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Cricket Shot Analysis',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
