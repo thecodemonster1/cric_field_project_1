f8a78e6ef924794903ac654b32c4b827cd3a291a
theCodeMonster1 <amharmohamed@icloud.com>
2025-03-26 02:34:24 +0530

    Remove commented-out code in main.dart to clean up the file structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,3 @@
-// import 'package:cric_field_project_1/Pages/HomePage.dart';
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home:  MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'Pages/InputPage.dart';
 import 'Pages/WagonWheelPage.dart';
